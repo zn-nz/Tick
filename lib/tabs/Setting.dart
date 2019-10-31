2e268ebe90ebcccb905334c5f2393343b9a6c083
lay-coder <nanmucoffin@163>
2019-10-31 18:44:08 +0800

    update

--- lib/tabs/Setting.dart
+++ lib/tabs/Setting.dart
@@ -14,34 +14,12 @@ class Setting extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.only(left: 20.0, right: 20.0),
           children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                print('单击');
-              },
-              onDoubleTap: () {
-                print('doubletap');
-              },
-              child: ListBody(children: <Widget>[
-                CircleAvatar(
-                  // radius: 100.0,
-                  backgroundImage: AssetImage("assets/images/avatar.png"),
-                ),
-              ]),
-            ),
-            GestureDetector(
-              onTap: () {
-                print('单击');
-              },
-              onDoubleTap: () {
-                print('doubletap');
-              },
-              child: Container(
-                width: 10.0,
-                height: 20.0,
-                child: CircleAvatar(
-                  // radius: 100.0,
-                  backgroundImage: AssetImage("assets/images/avatar.png"),
-                ),
+            ListTile(
+              title: Image.asset(
+                "assets/images/avatar.png",
+                width: 60.0,
+                height: 60.0,
+                alignment: Alignment.centerLeft,
               ),
             ),
           ],
